4db89ec615d0c14c18ded48ce65936cb81a6ac59
MrAle456 <alejandrojrl2@gmail.com>
2026-03-04 01:21:37 -0600

    Correccion de Errores
    El login se encuentra casi listo, solo ocupo colocar el icono y se empezara con el desarrollo de la homepage

--- flutter/island_water_impulsadores/lib/main.dart
+++ flutter/island_water_impulsadores/lib/main.dart
@@ -2,18 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:island_water_impulsadores/screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  //
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(useMaterial3: true),
-      home: LoginScreen(),
+      home: const LoginScreen(),
     );
   }
 }
