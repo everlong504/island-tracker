d4c508c2c866d1a123d1bf4a21a4db7a8996e86e
MrAle456 <alejandrojrl2@gmail.com>
2026-03-06 01:47:42 -0600

    Update home_page.dart

---
+++ flutter/island_water_impulsadores/lib/screens/home_page.dart
@@ -0,0 +1,40 @@
+<<<<<<< Updated upstream
+=======
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            radius: 1.5,
+            colors: [
+              const Color.fromARGB(255, 93, 224, 230),
+              const Color.fromARGB(255, 22, 135, 207),
+            ],
+          ),
+        ),
+
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Column(
+            children: [
+              Image(image: AssetImage('assets/transparentlogo.png')),
+              const SizedBox(height: 64),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+>>>>>>> Stashed changes
